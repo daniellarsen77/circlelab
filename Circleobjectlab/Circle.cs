ecbce0627a4ec0ef055e4f2d676bcb1dc08faeae
daniellarsen77 <daniellarsen127@gmail.com>
2023-04-21 22:44:59 -0400

    Finished lab

---
+++ Circleobjectlab/Circle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circleobjectlab
+{
+    using System;
+
+    public class Circle
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        public double CalculateDiameter()
+        {
+            return radius * 2;
+        }
+
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public void Grow()
+        {
+            radius *= 2;
+        }
+
+        public double GetRadius()
+        {
+            return radius;
+        }
+    }
+
+}
---
+++ Circleobjectlab/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Circleobjectlab
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            double radius;
+            Circle circle;
+
+            do
+            {
+                Console.Write("Enter the radius of the circle: ");
+                string input = Console.ReadLine();
+
+                if (!double.TryParse(input, out radius))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number with decimals.");
+                    continue;
+                }
+
+                circle = new Circle(radius);
+                break;
+            } while (true);
+
+            Console.WriteLine($"Diameter: {circle.CalculateDiameter():F2}");
+            Console.WriteLine($"Circumference: {circle.CalculateCircumference():F2}");
+            Console.WriteLine($"Area: {circle.CalculateArea():F2}");
+
+            do
+            {
+                Console.Write("Would you like to grow the circle? (yes/no): ");
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "yes" || input == "y")
+                {
+                    circle.Grow();
+                    Console.WriteLine($"Diameter: {circle.CalculateDiameter():F2}");
+                    Console.WriteLine($"Circumference: {circle.CalculateCircumference():F2}");
+                    Console.WriteLine($"Area: {circle.CalculateArea():F2}");
+                }
+                else if (input == "no" || input == "n")
+                {
+                    Console.WriteLine($"Goodbye! The final radius of the circle is {circle.GetRadius():F2}.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter \"yes\" or \"no\".");
+                }
+            } while (true);
+        }
+    }
+
+}
